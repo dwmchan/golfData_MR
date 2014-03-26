a03f91b4bde338209d3f21e1dd4c0c5d6ba3642d
Donald Chan <you@example.com>
2014-03-26 16:29:19 +1100

    Initial Commit

---
+++ golfData_MR/IEDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IEDAppDelegate.h
+//  golfData_MR
+//
+//  Created by Donald Chan on 26/03/2014.
+//  Copyright (c) 2014 iEndeavour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IEDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ golfData_MR/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  golfData_MR
+//
+//  Created by Donald Chan on 26/03/2014.
+//  Copyright (c) 2014 iEndeavour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IEDAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IEDAppDelegate class]));
+    }
+}
